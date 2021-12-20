dba73e25157615032d90c0a421c59e644fd7f1e7
Joshua Ashton <joshua@froggi.es>
2021-12-20 08:19:56 +0000

    steamcompmgr: Remove WORKAROUND_ZPOS_BUG
    This got fixed in libliftoff

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -345,24 +345,10 @@ static int g_nudgePipe[2] = {-1, -1};
 
 static LogScope xwm_log("xwm");
 
-// Right now there's a suuuper duper rare bug where if we change the zposes
-// from these values the overlay can render behind the base layer
-// or the override can render above the overlay which is very very wrong.
-// So for now, if we have both an override, just force composition
-// and keep these zpos values the same.
-#define WORKAROUND_ZPOS_BUG
-
-#ifdef WORKAROUND_ZPOS_BUG
-static const uint32_t g_zposBase = 0;
-static const uint32_t g_zposOverride = 0;
-static const uint32_t g_zposOverlay = 1;
-static const uint32_t g_zposCursor = 2;
-#else
 static const uint32_t g_zposBase = 0;
 static const uint32_t g_zposOverride = 1;
 static const uint32_t g_zposOverlay = 2;
 static const uint32_t g_zposCursor = 3;
-#endif
 
 // poor man's semaphore
 class sem
@@ -1588,14 +1574,8 @@ paint_all(Display *dpy, MouseCursor *cursor)
 #endif
 
 	bool bCapture = takeScreenshot || pw_buffer != nullptr;
-	
-#ifdef WORKAROUND_ZPOS_BUG
-	const bool bOverrideCompositeHack = override != nullptr;
-#else
-	const bool bOverrideCompositeHack = false;
-#endif
 
-	if ( BIsNested() == false && alwaysComposite == false && bCapture == false && bOverrideCompositeHack == false )
+	if ( BIsNested() == false && alwaysComposite == false && bCapture == false )
 	{
 		int ret = drm_prepare( &g_DRM, &composite, &pipeline );
 		if ( ret == 0 )
